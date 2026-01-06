342045e8e2d19e39eb54989a3ebfd2aa48160d40
Geun-Hwan <rmsghks1026@gmail.com>
2026-01-07 05:20:06 +0900

    test

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,11 +4,6 @@ import { Outlet } from "react-router-dom";
 import { AppHeader } from "./components/AppHeader";
 import Footer from "./components/Footer";
 
-/**
- * 애플리케이션의 메인 레이아웃 컴포넌트입니다.
- * 모든 페이지에 공통적으로 적용될 헤더(AppBar)와
- * 라우팅된 페이지 컨텐츠가 렌더링될 영역(Outlet)을 포함합니다.
- */
 function App() {
   return (
     <Box
